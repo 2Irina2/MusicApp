cadff4c339527f76ea20d04be6e3165249ad723c
irina <irina_22stefana@yahoo.com>
2017-05-10 00:10:52 +0300

    Third Commit

--- app/src/main/java/com/example/android/musicapp/NowPlayingClickListener.java
+++ app/src/main/java/com/example/android/musicapp/NowPlayingClickListener.java
@@ -1,18 +1,8 @@
 package com.example.android.musicapp;
 
-import android.content.Intent;
-import android.view.View;
-
-import static android.R.attr.start;
-
 /**
- * Created by irina on 07.05.2017.
+ * Created by irina on 09.05.2017.
  */
 
-public class NowPlayingClickListener implements View.OnClickListener{
-    @Override
-    public void onClick(View view){
-        Intent i = new Intent(MainActivity.this, NowPlayingActivity.class);
-        startActivity(i);
-    }
+public class NowPlayingClickListener {
 }
